ec2c40343880da1ae015d0014ad14faa16da7e4e
J2ghz <j2.00ghz@gmail.com>
2018-09-06 11:49:29 +0200

    comment loading

---
+++ src/CustomArmory/Wowhead.fs
@@ -0,0 +1,17 @@
+﻿module Wowhead
+
+open FSharp.Data
+
+let split (character:char) (string:string) =
+    string.Split(character)
+
+let splitTakeLast character =
+    split character >> Array.last
+
+let cssSelect selector (doc:FSharp.Data.HtmlDocument) =
+    CssSelectorExtensions.CssSelect(doc,selector)
+
+let getComment (url:string) =
+    let id = splitTakeLast '#' url
+    HtmlDocument.Load url
+    |> cssSelect (sprintf "#%swo" id)
