d93a712ccf4e6ccf978d06140ae16e1f42ddc39c
J2ghz <j2.00ghz@gmail.com>
2018-09-09 12:43:03 +0200

    conv

--- src/CustomArmory/Storylines.fs
+++ src/CustomArmory/Storylines.fs
@@ -13,7 +13,7 @@ type ProcessedStorylineItem =
 | ParallelStep of StepTitle * ProcessedStorylineItem list * ProcessedStorylineItem list
 | Achievement of AchievementId * AchievementEarned option
 | Quest of QuestId * Earned
-| Reputation of RepId * RepRequiredStanding * RepRequiredValue * Earned * RepEarnedStanding * RepEarnedValue
+| Reputation of RepId * RepRequiredStanding * RepRequiredValue * (Earned * RepEarnedStanding * RepEarnedValue) option
 | Level of LevelRequired * Earned * LevelEarned
 
 let eq a b = a = b
@@ -46,9 +46,19 @@ let rec fromData c = function
             Character.questCompleted c id
         )
     | StorylineData.Reputation(id,standing,value) ->
+        let rep = c.Reputation
+                |> Array.tryFind (fun rep -> rep.Id = id)
+                |> Option.map (fun r -> (r.Standing,r.Value))
+
         Reputation(
             id,
             standing,
             value,
-
+            rep |> Option.map (fun (s,v) ->
+                s >= standing && v >= value,
+                s,
+                v
+                )
         )
+    | StorylineData.Level(n) ->
+        Level( n, c.Level >= n, c.Level )
