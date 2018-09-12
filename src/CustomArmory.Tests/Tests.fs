a76fffc3738ae1bdcd322216f299d7bff64de44f
J2ghz <j2.00ghz@gmail.com>
2018-09-12 11:12:57 +0200

    add test

---
+++ src/CustomArmory.Tests/Tests.fs
@@ -0,0 +1,81 @@
+﻿module Tests
+
+open System
+open System.Net
+open System.Net.Http
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.TestHost
+open Microsoft.Extensions.DependencyInjection
+open Xunit
+
+// ---------------------------------
+// Test server/client setup
+// ---------------------------------
+
+let createHost() =
+    WebHostBuilder()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .Configure(Action<IApplicationBuilder> CustomArmory.App.configureApp)
+        .ConfigureServices(Action<IServiceCollection> CustomArmory.App.configureServices)
+
+// ---------------------------------
+// Helper functions
+// ---------------------------------
+
+let runTask task =
+    task
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+let get (client : HttpClient) (path : string) =
+    path
+    |> client.GetAsync
+    |> runTask
+
+let createRequest (method : HttpMethod) (path : string) =
+    let url = "http://127.0.0.1" + path
+    new HttpRequestMessage(method, url)
+
+let addCookiesFromResponse (response : HttpResponseMessage)
+                           (request  : HttpRequestMessage) =
+    request.Headers.Add("Cookie", response.Headers.GetValues("Set-Cookie"))
+    request
+
+let makeRequest (client : HttpClient) (request : HttpRequestMessage) =
+    use server = new TestServer(createHost())
+    use client = server.CreateClient()
+    request
+    |> client.SendAsync
+    |> runTask
+
+let ensureSuccess (response : HttpResponseMessage) =
+    if not response.IsSuccessStatusCode
+    then response.Content.ReadAsStringAsync() |> runTask |> failwithf "%A"
+    else response
+
+let isStatus (code : HttpStatusCode) (response : HttpResponseMessage) =
+    Assert.Equal(code, response.StatusCode)
+    response
+
+let isOfType (contentType : string) (response : HttpResponseMessage) =
+    Assert.Equal(contentType, response.Content.Headers.ContentType.MediaType)
+    response
+
+let readText (response : HttpResponseMessage) =
+    response.Content.ReadAsStringAsync()
+    |> runTask
+
+
+// ---------------------------------
+// Tests
+// ---------------------------------
+
+[<Fact>]
+let ``Test / is routed to index`` () =
+    use server = new TestServer(createHost())
+    use client = server.CreateClient()
+
+    get client "/"
+    |> isStatus HttpStatusCode.Redirect
